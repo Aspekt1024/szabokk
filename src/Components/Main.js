ef6b4519cfa657337616e3dc590fcf597f4f513c
Dan <d.szabo87@gmail.com>
2018-10-06 10:54:08 +1000

    Added Login class with static text

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,8 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import Login from './Login'
 
 class Main extends Component {
   render() {
-    return <div className="main">Main</div>
+    return (
+      <Fragment>
+        <div className="main">Main</div>
+        <Login />
+      </Fragment>
+    )
   }
 }
 
